8ccff60c3a2f1f08d4f22415c07d8e70ee9f3f4d
Sergios Lenis <sergioslenis@gmail.com>
2018-03-05 10:29:51 +0200

    Single job

--- lib/coding_exercise.rb
+++ lib/coding_exercise.rb
@@ -3,6 +3,10 @@ class CodingExercise
   def self.parce_jobs job
     if job.length ==0
       job
+    else
+      ret = job.split(' =>').join()
+
+
     end
   end
 end
--- test/test_coding_exercise.rb
+++ test/test_coding_exercise.rb
@@ -5,4 +5,9 @@ class CodingExerciseTest < Minitest::Test
   def test_empty_string
     assert_equal "" , CodingExercise.parce_jobs('')
   end
+
+  def test_single_job
+    assert_equal "a", CodingExercise.parce_jobs('a =>')
+  end
+
 end
